10e6345d0149cac0f7f303182b9c318cb9522695
Andrew Clarke <andrewclarke982@gmail.com>
2017-04-30 13:40:25 +0400

    Methods added

--- directory.rb
+++ directory.rb
@@ -12,11 +12,24 @@ students = [
 "Joffrey Baratheon",
 "Norman Bates"
 ]
-# and then print them
-puts "The students of Villians Academy"
-puts "-----------------------"
-students.each do |student|
-  puts student
+
+def print_header
+  puts "The students of Villians Academy"
+  puts "-----------------------"
 end
-# finally, we print the total
-puts "Overall, we have #{students.count} great students"
+
+def print(names)
+names.each do |name|
+  puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# nothing happens until we call method
+print_header
+print(students)
+print_footer(students)
+# note that above we are now passing the students variable to the methods as an argument (called names in both cases). This is becasue the methods dont have access to local variables defined outside them. 
